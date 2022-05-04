ee318bdd705ebfef5fdb96e35c70094cefb3a160
khozex <ismaelteles75@gmail.com>
2022-05-03 22:28:48 -0300

    Create First Test Seacher Service

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import {SeacherModule} from './Modules/SearcherModule/Seacher.module'
 async function bootstrap() {
   const app = await NestFactory.create(SeacherModule);
